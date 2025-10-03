96b15f2d51bb3b2bba1e3ce0ee3ea777a6cd623b
meglee0809 <meglee0809@highgateschool.org.uk>
2025-10-03 10:30:17 +0100

    dog

--- class_exercises/OOP/Dog OOP.py
+++ class_exercises/OOP/Dog OOP.py
@@ -1,1 +1,12 @@
 #dog class exercises
+
+class Dog:
+    def __init__(self,name,age): #__init__ is
+        self.name = name #self refers to the Dog itself
+        self.age = age
+
+    def speaks(self): #if dogname.speaks() is used it will return the below
+        return f'{self.name} says woof woof'
+
+
+grey = Dog('Grey',4)
