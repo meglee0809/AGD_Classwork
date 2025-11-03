86e9c5d10fc89e4a3c33a457f3d82f377ff1c5e8
meglee0809 <meglee0809@highgateschool.org.uk>
2025-11-03 16:00:43 +0000

    gamz

--- class_exercises/OOP/Fighting_Fantasy_OOP.py
+++ class_exercises/OOP/Fighting_Fantasy_OOP.py
@@ -16,7 +16,7 @@ class Character:
         self.score = None
 
     def __repr__(self): #this is dunder keys
-        return f"Character('{self.name}', skill={self.skill}, stamina={self.stamina}"
+        return f"Character('{self.name}', skill={self.skill}, stamina={self.stamina})"
 
     def find_score(self):
         self.roll = dice_sum(2)
@@ -32,17 +32,27 @@ class Character:
         if self.score > op.score:
             op.take_hit()
             print(f"{op.name} takes a hit!!")
+            print(f"-- {op.stamina} damage --")
+            result = "won"
+            return result
+
         #op wins
         elif self.score < op.score:
             self.take_hit()
             print(f"{self.name} got hit!!")
+            print(f"-- {self.stamina} damage --")
+            result = "lost"
+            return result
 
+        #draw
         else:
             self.take_hit(1)
             op.take_hit(1)
             print(f"You both got hit!!")
+            result = "draw"
+            return result
 
-    def return_char_status(self):
+    def return_character_status(self):
         return(f"{self.name} has {self.stamina} stamina !")
 
     def return_rolls_status(self):
@@ -65,13 +75,55 @@ class PlayerCharacter(Character):
         super().__init__(name,skill,stamina) #uses the super class init function
         self.luck = luck
 
-    @classmethod
+    @classmethod #modify a class state that would apply across all the instances of the class NOT a single character
     def generate_player_character(cls, name):
         skill = 6 + dice_sum(1)
         stamina = 12 + dice_sum(2)
         luck = 6 + dice_sum(1)
         return cls(name,skill,stamina,luck)
 
+    def __repr__(self): #this is dunder keys
+        return f"Character('{self.name}', skill={self.skill}, stamina={self.stamina}, luck={self.luck})"
+
+#characters-------------------------------------------------------------------------
+elizabeth = PlayerCharacter.generate_player_character("Elizabeth")
+
 kris = Character("Kris",11,13)
 dragon = Character("Rawry the dragon",10,22)
-flavia = PlayerCharacter.generate_player_character("Flavia")
+
+
+#game----------------------------------------------------------------------------------
+class Game:
+    """Game class - controls the Fighting Fantasy game class"""
+    @classmethod
+    def load_creatures(cls):
+        creatures = [Character("Rawry the dragon",10,22),
+                     Character("Jelly-fish",5,5),
+                     Character("Mildly Warm",8,5),
+                     Character("Sluuudge", 2, 10),
+                     ]
+        return creatures
+
+    def __init__(self):
+        self.op = None
+        self.player = None
+        self.round_result = None
+        self.creatures = self.load_creatures()
+
+    def choose_opponent(self):
+        self.op = random.choice(self.creatures)
+        self.creatures.remove(self.op)
+
+    def set_player(self,player_character):
+        self.player = player_character
+
+    def resolve_fight_round(self):
+        self.round_result = self.player.fight_round(self.op)
+
+    def return_character_status(self):
+        msg = f"{self.player.name} has skill {self.player.skill} and stamina {self.player.stamina}"
+
+
+game = Game()
+game.choose_opponent()
+game.set_player(elizabeth)
