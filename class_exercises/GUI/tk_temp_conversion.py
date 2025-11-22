77811d5b92ef546f654510847bd38fa3d657a2a9
meglee0809 <meglee0809@highgateschool.org.uk>
2025-11-22 18:02:54 +0000

    es ist sehr kalt

--- class_exercises/GUI/tk_temp_conversion.py
+++ class_exercises/GUI/tk_temp_conversion.py
@@ -1,19 +1,21 @@
 import tkinter as tk
 from tkinter import ttk
-from tk_temperature import *
+from tk_temperature import Temperature
+#from class_exercises.photos import GET PHOTOS
 
+#4:51-
 
 class Mainapp(tk.Tk):
     def __init__(self):
         super().__init__()
 
         self.Heading = Heading(self)
+        self.TemperatureFrame = TemperatureFrame(self)
+        self.PhotoFrame = PhotoFrame(self)
 
-        self.Temperatures = Temperatures(self)
-
-        self.Heading.grid(row=0, column=0, columnspan=3)
-
-        self.Temperatures.grid(row=2, column=0)
+        self.Heading.grid(row=0, column=0, columnspan=3, sticky="wnse")
+        self.TemperatureFrame.grid(row=1, column=0, columnspan=2, sticky="wnse")
+        self.PhotoFrame.grid(row=1, column=1, columnspan=1, sticky="wnse")
 
 
 class Heading(tk.Frame):
@@ -28,63 +30,78 @@ class Heading(tk.Frame):
         self.heading.grid(row=0, column=0, **settings)
 
 
-class EntryBox(tk.Frame):
+class TemperatureFrame(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
 
+        #first row -----------
         self.entry = tk.Entry(self)
+
+        self.current_option = tk.StringVar()
         self.combobox = ttk.Combobox(self)
+        self.combobox['values'] = ('Celsius', 'Fahrenheit', 'Kelvin')
+        self.combobox.set('+ select unit +')
+        self.current_option = self.combobox.get()
+        if self.entry.get() == "":
+            temp = 0
+        else:
+            temp = Temperature(int(self.entry.get()),self.current_option.lower())
+
+        #second row---------
+        # self.config(bg="midnight blue")
+        # USE AFTER -> text_color = "white"
+        self.Celsius = tk.Label(self, text="Celsius", font="Arial", justify=tk.LEFT)
+        self.Celsiusoutput = tk.Label(self, text=f"{'''temp.celsius'''}", font="Arial")
+
+        self.Fahrenheit = tk.Label(self, text="Fahrenheit", font="Arial", justify=tk.LEFT)
+        self.Fahrenheitoutput = tk.Label(self, text=f"{'''temp.fahrenheit'''}", font="Arial")
+
+        self.Kelvin = tk.Label(self, text="Kelvin", font="Arial", justify=tk.LEFT)
+        self.Kelvinoutput = tk.Label(self, text=f"{'''temp.kelvin'''}", font="Arial")
 
         self.place_widgets()
+        self.change_bg_colours()
+
 
     def place_widgets(self):
         settings = {'padx': 10, 'pady': 10, 'sticky': 'w'}
-        self.entry.grid(row=0, column=1, **settings)
+        self.entry.grid(row=0, column=0, **settings)
+        self.combobox.grid(row=0, column=1, **settings)
+
+        self.Celsius.grid(row=1, column=0, **settings)
+        self.Celsiusoutput.grid(row=1, column=1, **settings)
+        self.Fahrenheit.grid(row=2, column=0, **settings)
+        self.Fahrenheitoutput.grid(row=2, column=1, **settings)
+        self.Kelvin.grid(row=3, column=0, **settings)
+        self.Kelvinoutput.grid(row=3, column=1, **settings)
 
 
-class SubmitButton(tk.Frame):
+    def change_bg_colours(self):
+        if self.entry.get() == "":
+            colour_hex = f'#{255:02x}{255:02x}{255:02x}'
+        else:
+            if temp.celsius >= 10:
+                colour_hex = f'#{255-(temp.celsius%255):02x}{255-(temp.celsius%255):02x}{255:02x}'
+            elif temp.celsius < 10:
+                colour_hex = f'#{255:02x}{255 - (temp.celsius % 255):02x}{255 - (temp.celsius % 255):02x}'
+
+        self.master.config(bg=colour_hex)
+
+
+
+class PhotoFrame(tk.Frame): #GET PHOTOS
     def __init__(self, master):
         super().__init__(master)
-
-        self.submit = tk.Button(self, text="Submit",
-                                bg="gray67", fg="gray1",
-                                activebackground="red",
-                                activeforeground="white")
-        self.place_widgets()
-
-    def place_widgets(self):
-        settings = {'padx': 10, 'pady': 5, 'sticky': 'w'}
-        self.submit.grid(row=0, column=0, **settings)
-
-
-class Temperatures(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-
-        #self.config(bg="midnight blue")
-        # USE AFTER -> text_color = "white"
-        self.Celcius = tk.Label(self, text="Celcius", font="Arial", justify=tk.LEFT)
-        self.Celciusoutput = tk.Label(self, text=f"{temp.celsius}", font="Arial")
-
-        self.Fahrenheit = tk.Label(self, text="Fahrenheit", font="Arial", justify=tk.LEFT)
-        self.Fahrenheitoutput = tk.Label(self, text=f"{temp.fahrenheit}", font="Arial")
-
-        self.Kelvin = tk.Label(self, text="Kelvin", font="Arial")
-        self.Kelvinoutput = tk.Label(self, text=f"{temp.kelvin}", font="Arial")
-
-        self.place_widgets()
-
-    def place_widgets(self):
-        settings = {'padx': 10, 'pady': 5, 'sticky': 'w'}
-        self.Celcius.grid(row=0, column=0, **settings)
-        self.Celciusoutput.grid(row=0, column=1, **settings)
-        self.Fahrenheit.grid(row=1, column=0, **settings)
-        self.Fahrenheitoutput.grid(row=1, column=1, **settings)
-        self.Kelvin.grid(row=2, column=0, **settings)
-        self.Kelvinoutput.grid(row=2, column=1, **settings)
-
-
-
+'''
+        if temp.celsius >= 22:
+            self.photo = tk.PhotoImage(file="hawt.png")
+        elif temp.celsius < 4:
+            self.photo = tk.PhotoImage(file="kalt.png")
+        else:
+            self.photo = tk.PhotoImage(file="happikitty.png")
+            
+        self.photo.grid(row=0, column=0, padx=10, pady=10)
+'''
 
 
 
@@ -92,6 +109,7 @@ if __name__ == '__main__':
 
     app = Mainapp()
     app.title("THE temperature conversion app")
+    temp = Temperature(25,"celsius")
     app.geometry('550x500+100+100')
 
     app.mainloop()
---
+++ class_exercises/GUI/tk_temperature.py
@@ -0,0 +1,50 @@
+class Temperature:
+    def __init__(self, *args, **kwargs):
+        if args:
+            self._celsius = args[0]
+        elif 'celsius' in kwargs:
+            self.celsius = kwargs['celsius']
+        elif 'fahrenheit' in kwargs:
+            self.fahrenheit = kwargs['fahrenheit']
+        elif 'kelvin' in kwargs:
+            self.kelvin = kwargs['kelvin']
+        else:
+            raise TypeError('Temperature in celsius, fahrenheit or kelvin must be specified')
+
+
+    @property
+    def celsius(self):
+        return f'{self._celsius} \u00B0C'
+
+    @celsius.setter
+    def celsius(self, value: float):
+        if value < -273.15:
+            raise ValueError('Celsius value must be greater than absolute zero (-273.15 \u00B0C)')
+        self._celsius = value
+
+    @property
+    def fahrenheit(self)  -> float:
+        fahrenheit = (self._celsius * 9 / 5) + 32
+        return fahrenheit
+
+    @fahrenheit.setter
+    def fahrenheit(self, value: float):
+        self._celsius = (value - 32) * 5/9
+
+    @property
+    def kelvin(self) -> float:
+        kelvin = self._celsius + 273.15
+        return kelvin
+
+    @kelvin.setter
+    def kelvin(self, value: float):
+        self._celsius = value - 273.15
+
+    def __repr__(self):
+        return f"Temperature(celsius={self._celsius:.1f})"
+
+if __name__ == '__main__':
+    temp = Temperature(25,"celcius")
+    # Note PyCharm is warning me not to do this!
+    print(temp._celsius)
+    print(temp.fahrenheit)
