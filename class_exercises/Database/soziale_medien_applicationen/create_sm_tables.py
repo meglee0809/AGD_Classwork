bf43d3de515c4e562e1d302eabfb1717228d5400
meglee0809 <meglee0809@highgateschool.org.uk>
2026-01-08 10:17:37 +0000

    SOZIALE

---
+++ class_exercises/Database/soziale_medien_applicationen/create_sm_tables.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+create_users_table = """
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    age INTEGER,
+    gender TEXT,
+    nationality TEXT
+);
+"""
+
+create_posts_table = """
+CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+"""
+
+create_likes_table = """
+CREATE TABLE IF NOT EXISTS likes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    post_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (post_id) REFERENCES posts (id)
+);
+"""
+
+create_comments_table = """
+CREATE TABLE IF NOT EXISTS comments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    text TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    post_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (post_id) REFERENCES posts (id)
+);
+"""
+with sqlite3.connect('soziale_medien.db') as conn:
+    cursor = conn.cursor()
+    cursor.execute(create_users_table)
+    cursor.execute(create_posts_table)
+    cursor.execute(create_likes_table)
+    cursor.execute(create_comments_table)
+    conn.commit()
+
