67a4876ebb9af6ee3b16e5a79e03594c122c3d80
meglee0809 <meglee0809@highgateschool.org.uk>
2026-01-12 23:40:31 +0000

    .::>{:"L:"">';">/.;'   ><>

--- class_exercises/Database/soziale_medien_applicationen/write_to_sm.py
+++ class_exercises/Database/soziale_medien_applicationen/write_to_sm.py
@@ -2,13 +2,32 @@ import sqlite3
 conn = sqlite3.connect("soziale_medien.db")
 
 users_data = [('James', 25, 'male', 'USA'), ('Leila', 32, 'female', 'France'), ('Brigitte', 35, 'female', 'England'), ('Mike', 40, 'male', 'Denmark'), ('Elizabeth', 21, 'female', 'Canada'),]
+posts_data = [('Happy', 'I am feeling very happy today', 1), ('Hot Weather', 'The weather is very hot today', 2), ('Help', 'I need some help with my work', 2), ('Great News', 'I am getting married', 1), ('Interesting Game', 'It was a fantastic game of tennis', 5), ('Party', 'Anyone up for a late-night party today?', 3)]
+comments_data = [('Count me in', 1, 6), ('What sort of help?', 5, 3), ('Congrats buddy', 2, 4), ('I was rooting for Nadal though', 4, 5), ('Help with your thesis?', 2, 3), ('Many congratulations', 5, 4)]
+likes_data = [(1, 6), (2, 3), (1, 5), (5, 4), (2, 4), (4, 2), (3, 6)]
 
 cursor = conn.cursor()
-parameterised_insert_query = """
+
+parameterised_insert_user_query = """
 INSERT INTO users (Name, Age, Gender, Nationality) 
     VALUES (?, ?, ?, ?);
 """
+parameterised_insert_posts_query = """
+INSERT INTO posts (Title, Description, User_ID) 
+    VALUES (?, ?, ?);
+"""
+parameterised_insert_comments_query = """
+INSERT INTO comments (Text, User_Id, Post_ID)
+    VALUES (?, ?, ?);
+"""
+parameterised_insert_likes_query = """
+INSERT INTO likes (User_ID, Post_ID)
+    VALUES (?, ?);
+"""
 
-cursor.executemany(parameterised_insert_query,users_data)
+#cursor.executemany(parameterised_insert_query,users_data)
+#cursor.executemany(parameterised_insert_posts_query,posts_data)
+#cursor.executemany(parameterised_insert_comments_query,comments_data)
+cursor.executemany(parameterised_insert_likes_query, likes_data)
 conn.commit() #DONT FORGET
 conn.close()
