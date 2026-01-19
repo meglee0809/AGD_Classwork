a25f67127f1301f1db84bfa5499ea13b2ba43712
meglee0809 <meglee0809@highgateschool.org.uk>
2026-01-19 15:12:09 +0000

    a

---
+++ class_exercises/Database/add_table.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from models import Person
+
+
+# Create some instances of the Person class
+andrew = Person(first_name="Andrew", last_name="Dales")
+people = [Person(first_name="Tom", last_name="Noyce"), Person(first_name='Aurora', last_name="Laws")]
+
+
+# Connect to the activities database
+engine = create_engine('sqlite:///activities.sqlite', echo=True)
+
+
+# Create a session and add the people to the database
+with Session(engine) as sess:
+    sess.add(andrew)
+    sess.add_all(people)
+    sess.commit()
